7a9b108c3b902e7b2a9c8c743f0d2c08c6640750
David C Ellis <ducksual@gmail.com>
2024-01-18 16:24:33 +0000

    Don't include pyenv pythons twice if they are registered for Py usage too.

--- src/ducktools/pythonfinder/win32/__init__.py
+++ src/ducktools/pythonfinder/win32/__init__.py
@@ -3,8 +3,17 @@ from .registry_search import get_registered_pythons
 
 
 def get_python_installs():
+    registered_pythons = get_registered_pythons()
+    pyenv_pythons = get_pyenv_pythons()
+
+    # Pyenv pythons should only be listed if they are not already included
+    registered_exes = {
+        p.executable for p in registered_pythons
+    }
+    pythons = registered_pythons + [p for p in pyenv_pythons if p.executable not in registered_exes]
+
     return sorted(
-        get_pyenv_pythons() + get_registered_pythons(),
+        pythons,
         key=lambda x: x.version,
         reverse=True,
     )
