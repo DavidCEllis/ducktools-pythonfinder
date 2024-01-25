8e186edee46751bbf2924598c38707b65fe6ad01
David C Ellis <ducksual@gmail.com>
2024-01-25 18:00:21 +0000

    Fix typo using the wrong function

--- src/ducktools/pythonfinder/win32/__init__.py
+++ src/ducktools/pythonfinder/win32/__init__.py
@@ -7,4 +7,4 @@ from .registry_search import get_registered_pythons
 
 def get_python_installs() -> Iterator[PythonInstall]:
     yield from get_registered_pythons()
-    yield from get_python_installs()
+    yield from get_pyenv_pythons()
