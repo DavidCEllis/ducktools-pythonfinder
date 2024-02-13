a160d8a13c45bcb28c0a2daf1dbce5ace3549bc3
David C Ellis <ducksual@gmail.com>
2024-02-13 17:39:52 +0000

    Test get_folder_pythons function

--- tests/test_foldersearch.py
+++ tests/test_foldersearch.py
@@ -13,13 +13,15 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from unittest.mock import patch, MagicMock
+import sys
+import os.path
+from unittest.mock import patch, MagicMock, call
 import textwrap
 import subprocess
 
 import pytest
 
-from ducktools.pythonfinder.shared import get_install_details, PythonInstall
+from ducktools.pythonfinder.shared import get_install_details, PythonInstall, get_folder_pythons
 from ducktools.pythonfinder import details_script
 
 fake_details_out = PythonInstall(
@@ -43,7 +45,7 @@ fake_details = textwrap.dedent("""
 
 @pytest.mark.parametrize("output, expected", [(fake_details, fake_details_out), ("InvalidJSON", None)])
 def test_get_install_details(output, expected):
-    with patch("ducktools.pythonfinder.shared._laz.subprocess.run") as run_mock:
+    with patch("subprocess.run") as run_mock:
         mock_out = MagicMock()
 
         mock_out.stdout = output
@@ -63,7 +65,7 @@ def test_get_install_details(output, expected):
 
 def test_get_install_details_error():
     with patch(
-        "ducktools.pythonfinder.shared._laz.subprocess.run",
+        "subprocess.run",
         side_effect=subprocess.CalledProcessError(1, "Unsuccessful Call")
     ) as run_mock:
 
@@ -77,3 +79,40 @@ def test_get_install_details_error():
         )
 
         assert details is None
+
+
+def test_get_folder_pythons(fs):
+    func = "ducktools.pythonfinder.shared.get_install_details"
+
+    if sys.platform == "win32":
+        fld = "C:\\temp\\python"
+        python_exe = os.path.join(fld, "python.exe")
+        pypy_exe = os.path.join(fld, "pypy.exe")
+        non_python_file = os.path.join(fld, "python3-futurize.exe")
+    else:
+        fld = "~/temp/python"
+        python_exe = os.path.join(fld, "python")
+        pypy_exe = os.path.join(fld, "pypy")
+        non_python_file = os.path.join(fld, "python3-futurize")
+
+    fs.create_dir(fld)
+
+    fs.create_file(python_exe)
+    fs.create_file(pypy_exe)
+    fs.create_file(non_python_file)
+
+    def mock_func(pth):
+        return pth
+
+    with patch(func, side_effect=mock_func) as get_dets:
+        result = list(get_folder_pythons(fld))
+
+        get_dets.assert_has_calls(
+            [
+                call(python_exe),
+                call(pypy_exe),
+            ],
+            any_order=True
+        )
+
+    assert result == [python_exe, pypy_exe]
