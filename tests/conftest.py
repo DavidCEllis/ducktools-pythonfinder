b4f7604e74719bea60e0797b0e7f1a8ef54d547b
David C Ellis <ducksual@gmail.com>
2024-09-24 17:18:43 +0100

    Add some commands to test the UV python finder

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,3 +35,26 @@ def sources_folder():
 @pytest.fixture
 def uses_details_script(fs):
     fs.add_real_file(details_script.__file__)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--run-uv-python",
+        action="store_true",
+        default=False,
+        help="Run tests that involve installing UV pythons",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "uv_python: only run test if --run-uv-python is specified"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if not config.getoption("--run-uv-python"):
+        skipper = pytest.mark.skip(reason="Only run when --run-uv-python is given")
+        for item in items:
+            if "uv_python" in item.keywords:
+                item.add_marker(skipper)
